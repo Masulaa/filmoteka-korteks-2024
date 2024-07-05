eb000384c883e5ef8150704853ad3af98dc7459a
jovan <git.keseljevicjovan@proton.me>
2024-07-05 23:46:07 +0200

    hotfix: movie image

--- resources/views/movieslist.blade.php
+++ resources/views/movieslist.blade.php
@@ -9,7 +9,7 @@
                 <div class="group relative">
                     <div
                         class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
-                        <img src="https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg"
+                        <img src="{{$movie->image}}"
                             alt="Front of men&#039;s Basic Tee in black."
                             class="h-full w-full object-cover object-center lg:h-full lg:w-full">
                     </div>
