effc496b4af871335cf6cc472692fc0e23a56d32
jovan <git.keseljevicjovan@proton.me>
2024-07-06 00:53:13 +0200

    add home and movie routes in middleware, add rememberToken in users table

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,13 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    Route::get('/home', [MovieController::class, 'index'])->name('home');
+    Route::get('/movie', [MovieController::class, 'show'])->name('movie');
+
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->middleware('verified')->name('dashboard');
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
