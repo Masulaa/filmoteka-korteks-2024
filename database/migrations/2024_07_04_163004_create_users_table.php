31f001f80203653d4beab8738c9e07b02c357106
Luka <lukamasulovic2@gmail.com>
2024-07-05 11:44:15 +0200

    fixed migrations(sessions)

--- database/migrations/2024_07_04_163004_create_users_table.php
+++ database/migrations/2024_07_04_163004_create_users_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
@@ -15,21 +14,11 @@ return new class extends Migration
             $table->string('password', 255);
             $table->timestamps();
         });
-        // За Middlewire
-        Schema::create('sessions', function (Blueprint $table) {
-            $table->string('id')->primary();
-            $table->foreignId('user_id')->nullable()->index();
-            $table->string('ip_address', 45)->nullable();
-            $table->text('user_agent')->nullable();
-            $table->timestamps();
 
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-        });
     }
     public function down(): void
     {
         Schema::dropIfExists('users');
-        Schema::dropIfExists('sessions');
     }
 };
 
