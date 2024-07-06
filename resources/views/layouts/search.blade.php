dd63d3d688300e211a370a98d0784e5cd5f439ea
Luka <lukamasulovic2@gmail.com>
2024-07-07 00:13:09 +0200

    ignore

--- resources/views/layouts/search.blade.php
+++ resources/views/layouts/search.blade.php
@@ -1,5 +1,5 @@
 <div class="flex flex-col items-center justify-center my-4">
-    <div class="relative w-96">
+    <div class="relative w-208">
         <div class="absolute inset-y-0 flex items-center pointer-events-none start-0 ps-3">
             <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                 <path stroke="white" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
@@ -10,7 +10,7 @@
             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="Search..." required />
     </div>
-    <div class=" text-slate-800 dark:text-white">
+    <div class="text-slate-800 dark:text-white">
         <ul id="movie_data" class="flex flex-col gap-3 p-4 max-w-96">
 
         </ul>
@@ -20,7 +20,7 @@
 <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 <script>
-    $(document).ready(function() {
+    $(document).ready(function () {
         // CSRF token setup
         $.ajaxSetup({
             headers: {
@@ -42,17 +42,17 @@
                     query: query
                 },
                 dataType: 'json',
-                success: function(data) {
+                success: function (data) {
                     $('#movie_data').html(data.html);
                 },
-                error: function(xhr, status, error) {
+                error: function (xhr, status, error) {
                     console.error(xhr.responseText);
                 }
             });
         }
 
         // Event listener for keyup in search input
-        $(document).on('keyup', '#search', function() {
+        $(document).on('keyup', '#search', function () {
             var query = $(this).val().trim(); // Trim whitespace from the input
             fetch_movie_data(query);
         });
