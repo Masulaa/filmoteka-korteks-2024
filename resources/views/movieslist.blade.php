13d330ee7a7e687182508178309bde2d4aa87eb1
Luka <lukamasulovic2@gmail.com>
2024-07-06 18:58:30 +0200

    fixed title in home page

--- resources/views/movieslist.blade.php
+++ resources/views/movieslist.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-    <h2 class="text-2xl font-bold tracking-tight text-gray-900">Movies</h2>
+    <h2 class="text-2xl font-bold tracking-tight text-white">Movies</h2>
 
     <div class="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
         @foreach ($movies as $movie)
