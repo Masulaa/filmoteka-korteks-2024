9edb6e68114484c3f0b26ca9f7b4b142cae09b09
jovan <git.keseljevicjovan@proton.me>
2024-07-06 16:50:37 +0200

    update TMDbService (display genre as string, not number), update movies:sync command to bypass API limit per request

--- routes/console.php
+++ routes/console.php
@@ -9,11 +9,27 @@ use App\Models\Movie;
 #    $this->comment(Inspiring::quote());
 #})->purpose('Display an inspiring quote')->hourly();
 
+/*
+ * TMDb API изгледа има ограничење од 94 филма по позиву,
+ * зато нека се синхронизација врши у више итерација од по 94 филма.
+*/
+
 Artisan::command('movies:sync {count}', function ($count) {
     $this->comment("Synchronizing {$count} movies from TMDb...");
 
     $tmdbService = new TMDbService();
-    $fetchedCount = $tmdbService->fetchPopularMovies($count);
 
-    $this->info("Successfully synchronized {$fetchedCount} movies!");
+    $requestsNeeded = ceil($count / 94);
+
+    $remainingMovies = $count;
+
+    for ($i = 0; $i < $requestsNeeded; $i++) {
+        $moviesToSync = min(94, $remainingMovies); 
+        $fetchedCount = $tmdbService->fetchPopularMovies($moviesToSync); 
+        $remainingMovies -= $fetchedCount;
+
+        $this->info("Successfully synchronized {$fetchedCount} movies (part " . ($i+1) . "/{$requestsNeeded})");
+    }
+
+    $this->info("Finished synchronizing {$count} movies.");
 })->purpose('Synchronize movies from TMDb')->daily();
