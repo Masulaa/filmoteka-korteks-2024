b447dbee91e5434de4ed6af625e0e71b181200dd
lucic15 <lucicluka31@gmail.com>
2024-07-06 02:28:21 +0200

    Footer and Header have been summoned.

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,5 @@
+<!-- resources/views/layouts/footer.blade.php -->
+<footer class="bg-white dark:bg-gray-800 dark:text-white p-3 border-b border-gray-100 dark:border-gray-700 text-center">
+    <p>/* Super cool footer */</p>
+    <p>Linux 🔝</p>
+</footer>
