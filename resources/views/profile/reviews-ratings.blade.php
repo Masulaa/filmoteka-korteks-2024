b3ebf682c51b8b72954a59cfba01c03fdd67c0a1
lucic15 <lucicluka31@gmail.com>
2024-07-09 21:15:50 +0200

    Better Reviews

--- resources/views/profile/reviews-ratings.blade.php
+++ resources/views/profile/reviews-ratings.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 @section('content')
-    <div class="container max-w-4xl px-4 py-8 mx-auto my-6">
+    <div class="container max-w-xl px-4 py-8 mx-auto my-6">
         <h1 class="mb-8 text-3xl font-bold text-white">Your Reviews and Ratings</h1>
 
         <div class="mb-12">
@@ -8,9 +8,20 @@
             @if ($reviews->count())
                 <ul class="space-y-6">
                     @foreach ($reviews as $review)
-                        <li class="p-6 bg-white rounded-lg shadow-md">
-                            <h3 class="mb-2 text-xl font-bold text-gray-900">{{ $review->movie->title }}</h3>
-                            <p class="text-gray-600">{{ $review->content }}</p>
+                        <li class="p-4 bg-white rounded-lg shadow-md">
+                            <a href="{{ route('movie', $review->movie->id) }}" class="flex items-center space-x-4">
+                                <img src="{{ $review->movie->image }}" alt="{{ $review->movie->title }}"
+                                    class="object-cover w-20 rounded-lg shadow-md">
+                                <div>
+                                    <h3 class="text-xl font-bold text-gray-900">{{ $review->movie->title }}</h3>
+                                    <p class="text-sm text-gray-600 ">Director: {{ $review->movie->director }}</p>
+                                    <p class="text-sm text-gray-600 ">Release Date: {{ $review->movie->release_date }}
+                                    </p>
+                                    <p class="text-sm text-gray-600">Date: {{ $review->created_at }}
+
+                                    <p class="mt-4 text-gray-700">Your Review: {{ $review->content }}</p>
+                                </div>
+                            </a>
                         </li>
                     @endforeach
                 </ul>
@@ -24,10 +35,21 @@
             @if ($ratings->count())
                 <ul class="space-y-6">
                     @foreach ($ratings as $rating)
-                        <li class="p-6 bg-white rounded-lg shadow-md">
-                            <h3 class="mb-2 text-xl font-bold text-gray-900">{{ $rating->movie->title }}</h3>
-                            <p class="text-gray-600">Rating: <span
-                                    class="font-semibold text-indigo-600">{{ $rating->rating }}/10</span></p>
+                        <li class="p-4 bg-white rounded-lg shadow-md">
+                            <a href="{{ route('movie', $rating->movie->id) }}" class="flex items-center space-x-4">
+                                <img src="{{ $rating->movie->image }}" alt="{{ $rating->movie->title }}"
+                                    class="object-cover w-20 rounded-lg shadow-md">
+                                <div>
+                                    <h3 class="text-xl font-bold text-gray-900">{{ $rating->movie->title }}</h3>
+                                    <p class="text-sm text-gray-600">Director: {{ $rating->movie->director }}</p>
+                                    <p class="text-sm text-gray-600">Release Date: {{ $rating->movie->release_date }}
+                                    </p>
+                                    <p class="text-sm text-gray-600">Date: {{ $rating->created_at }}
+                                    </p>
+                                    <p class="mt-4 text-gray-700">Your Rating: <span
+                                            class="font-semibold text-indigo-600">{{ $rating->rating }}/10</span></p>
+                                </div>
+                            </a>
                         </li>
                     @endforeach
                 </ul>
