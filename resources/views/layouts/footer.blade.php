69d3af6a55c042dbf40e76964bed0f8d6f145d63
Luka <lukamasulovic2@gmail.com>
2024-07-09 11:52:00 +0200

    fixed footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -3,10 +3,11 @@
         <div class="sm:flex sm:items-center sm:justify-between">
             <a href="" class="flex items-center mb-4 space-x-3 sm:mb-0 rtl:space-x-reverse">
                 {{-- <img src="" class="h-8" alt="Logo" /> --}}
-                {{-- <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">THB Movies</span> --}}
+                {{-- <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">THB
+                    Movies</span> --}}
                 <x-application-logo class="block w-auto text-gray-800 fill-current h-9 dark:text-gray-200" />
             </a>
-            <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+            <ul class="flex flex-wrap items-center mb-4 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
                 <li>
                     <a href="#" class="hover:underline me-4 md:me-6">About</a>
                 </li>
@@ -21,10 +22,8 @@
                 </li>
             </ul>
         </div>
-        <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-        <div class="flex flex-col block text-sm text-gray-500 sm:text-center dark:text-gray-400">/* Super cool footer */
-            <a href="#" class="hover:underline">Linux 🔝</a>
-        </div>
+        <!-- <hr class="my-5 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" /> -->
+
 
     </div>
 </footer>
