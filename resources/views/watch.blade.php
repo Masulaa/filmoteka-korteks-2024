606b5a34da70be21c5db2a99abae9075e1939254
Luka <lukamasulovic2@gmail.com>
2024-07-09 23:58:45 +0200

    new stuff in watch movie blade

--- resources/views/watch.blade.php
+++ resources/views/watch.blade.php
@@ -13,6 +13,20 @@
                 style="width: 70rem; height: 33rem; overflow: hidden;"></iframe>
         </div>
         <div class="text-white ml-10 mb-4">
+            <div class="flex items-center mt-3">
+                <svg class="flex-shrink-0 w-5 h-5 text-yellow-400" viewBox="0 0 20 20" fill="currentColor">
+                    <path
+                        d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                </svg>
+                <p class="ml-1 text-sm text-gray-600 dark:text-gray-400">
+                    {{ $movie->averageRating() }} ({{ $movie->countRatings() }} ratings)
+                </p>
+                <ul class="flex flex-row items-center sm:gap-[14px] xs:gap-3 gap-[6px] flex-wrap ml-3 ">
+                    @foreach (explode(',', $movie->genre) as $genre)
+                        <li class="px-3 py-1 text-sm text-white bg-gray-800 rounded-full">{{ trim($genre) }}</li>
+                    @endforeach
+                </ul>
+            </div>
             <h2>For better experience use addblocker, e.g. <a href="https://ublockorigin.com/" target="_blank"
                     rel="noopener noreferrer" class="underline">uBlock Origin</a></h2>
         </div>
