b687e723f3bdb04ff7def7c234a8e889c72577eb
Luka <lukamasulovic2@gmail.com>
2024-07-10 00:03:28 +0200

    new logo

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,5 +1,6 @@
 <div class="flex">
     {{-- <img src="https://i.ibb.co/CvSbtDL/Untitled.png" width="64" height="64"> --}}
+    <img src="https://i.ibb.co/7Vy4bJ6/krug.png" width="50" height="50">
     <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Movies</span>
 
 
