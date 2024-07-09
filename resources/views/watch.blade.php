a3069ea0c47c380dc93312631b9786671535aa3c
Luka <lukamasulovic2@gmail.com>
2024-07-09 17:27:00 +0200

    fixed css on video page

--- resources/views/watch.blade.php
+++ resources/views/watch.blade.php
@@ -1,21 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <h1 class="text-white">You are watching: {{ $movie->title }}</h1>
+<div class="container">
+    <h1 class="text-3xl font-bold tracking-tight ml-10 mt-4 text-white">{{ $movie->title }}</h1>
 
-        @if ($movie->video_id)
-            <div class="text-white">
-                <h2>За најбоље искуство користите adblocker као што је <a href="https://ublockorigin.com/" target="_blank" rel="noopener noreferrer" class="underline">uBlock Origin</a></h2>
-            </div>
+    @if ($movie->video_id)
 
-            <div class="embed-responsive embed-responsive-16by9 mt-4">
-                <iframe class="embed-responsive-item" src="https://vidsrc.pro/embed/movie/{{ $movie->video_id }}" allowfullscreen
-                        allow="autoplay; fullscreen" allowfullscreen="yes" frameborder="no" scrolling="no"
-                        style="width: 100%; height: 1000px; overflow: hidden;"></iframe>
-            </div>
-        @else
-            <p class="text-red-500">За сада овај видео је недоступан. Борис је крив</p>
-        @endif
-    </div>
+
+        <div class="embed-responsive embed-responsive-16by9 mt-4 mb-4 flex justify-center">
+            <iframe class="embed-responsive-item" src="https://vidsrc.pro/embed/movie/{{ $movie->video_id }}"
+                allowfullscreen allow="autoplay; fullscreen" allowfullscreen="yes" frameborder="no" scrolling="no"
+                style="width: 70rem; height: 33rem; overflow: hidden;"></iframe>
+        </div>
+        <div class="text-white ml-10 mb-4">
+            <h2>For better experience use addblocker, e.g. <a href="https://ublockorigin.com/" target="_blank"
+                    rel="noopener noreferrer" class="underline">uBlock Origin</a></h2>
+        </div>
+    @else
+        <p class="text-red-500">This video is not available at the moment...</p>
+    @endif
+</div>
 @endsection
