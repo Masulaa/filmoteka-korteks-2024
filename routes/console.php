5c09482c91f6b8f7630e94dfc5531a2c2e680bc8
jovan <git.keseljevicjovan@proton.me>
2024-07-18 13:02:42 +0200

    update console.php

--- routes/console.php
+++ routes/console.php
@@ -1,16 +1,11 @@
 <?php
 
-//use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
+
 /**
  * Command to synchronize movies from TheMovieDB.
  * @param int|null $count
+ * @param int|null $consoleOutput
  * @return void
  */
- /*
-Artisan::command('synchronize:movies', function () {
-    Artisan::call('movies:sync',['consoleOutput'=> 0]);
-})->purpose('Synchronize movies from TheMovieDB')->dailyAt("03:00");
-*/
-
 Schedule::command('movies:sync 0 0')->dailyAt("03:00"); 
