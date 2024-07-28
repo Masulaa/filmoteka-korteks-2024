f8847725bd58d060af5c08804f86429a0abaf3a3
Luka <lukamasulovic2@gmail.com>
2024-07-28 21:57:55 +0200

    profile controller refactored

---
+++ app/Http/Controllers/ProfileReviewsAndRatingsController.php
@@ -0,0 +1,28 @@
+<?php
+
+
+
+namespace App\Http\Controllers;
+
+use App\Models\{MovieRating, MovieReview};
+use Illuminate\Support\Facades\Auth;
+use Illuminate\View\View;
+
+class ProfileReviewsAndRatingsController extends Controller
+{
+
+    /**
+     * Display the user's reviews and ratings.
+     *
+     * @return View
+     */
+    public function reviewsAndRatings(): View
+    {
+        $user = Auth::user();
+
+        $reviews = MovieReview::where('user_id', $user->id)->with('movie')->get();
+        $ratings = MovieRating::where('user_id', $user->id)->with('movie')->get();
+
+        return view('profile.reviews-ratings', compact('reviews', 'ratings'));
+    }
+}
