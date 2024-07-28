3830005e8ed0868980e674542695eb0f80d8423b
jovan <git.keseljevicjovan@proton.me>
2024-07-28 22:53:03 +0200

    Fix serie-genre sync issue

--- app/Services/MoviesService.php
+++ app/Services/MoviesService.php
@@ -7,9 +7,9 @@ use App\Models\{Movie, MovieCast};
 
 class MoviesService
 {
-    public function __construct(
-        protected TMDbService $tmdbService
-    ) {}
+    public function __construct(protected TMDbService $tmdbService)
+    {
+    }
 
     /**
      * Get the number of all movies.
@@ -18,7 +18,12 @@ class MoviesService
      * @throws GuzzleException
      */
     public function getNumberOfAllMovies(): int
-    { return $this->tmdbService->fetchData('https://api.themoviedb.org/3/movie/popular', ['page' => 1])['total_results'] ?? 0; }
+    {
+        return $this->tmdbService->fetchData(
+            "https://api.themoviedb.org/3/movie/popular",
+            ["page" => 1]
+        )["total_results"] ?? 0;
+    }
 
     /**
      * Get YouTube trailer ID for the given movie.
@@ -29,10 +34,15 @@ class MoviesService
      */
     public function getYouTubeTrailerId(int $movieId): ?string
     {
-        $videos = $this->tmdbService->fetchData("https://api.themoviedb.org/3/movie/{$movieId}/videos", [])['results'] ?? [];
+        $videos =
+            $this->tmdbService->fetchData(
+                "https://api.themoviedb.org/3/movie/{$movieId}/videos",
+                []
+            )["results"] ?? [];
         foreach ($videos as $video) {
-            if ($video['site'] === 'YouTube' && $video['type'] === 'Trailer')
-                return $video['key'];
+            if ($video["site"] === "YouTube" && $video["type"] === "Trailer") {
+                return $video["key"];
+            }
         }
         return null;
     }
@@ -46,12 +56,17 @@ class MoviesService
      */
     public function getDirector(int $movieId): string
     {
-        $crew = $this->tmdbService->fetchData("https://api.themoviedb.org/3/movie/{$movieId}/credits", [])['crew'] ?? [];
+        $crew =
+            $this->tmdbService->fetchData(
+                "https://api.themoviedb.org/3/movie/{$movieId}/credits",
+                []
+            )["crew"] ?? [];
         foreach ($crew as $member) {
-            if ($member['job'] === 'Director')
-                return $member['name'];
+            if ($member["job"] === "Director") {
+                return $member["name"];
+            }
         }
-        return 'unknown director';
+        return "unknown director";
     }
 
     /**
@@ -63,13 +78,23 @@ class MoviesService
      */
     public function getCast(int $movieId): array
     {
-        $cast = array_slice($this->tmdbService->fetchData("https://api.themoviedb.org/3/movie/{$movieId}/credits", [])['cast'] ?? [], 0, 10);
-        return array_map(fn($actor) => [
-            'name' => $actor['name'],
-            'character' => $actor['character'],
-            'profile_path' => $this->getUrl($actor['profile_path'], 'w185'),
-            'id' => $actor['id']
-        ], $cast);
+        $cast = array_slice(
+            $this->tmdbService->fetchData(
+                "https://api.themoviedb.org/3/movie/{$movieId}/credits",
+                []
+            )["cast"] ?? [],
+            0,
+            10
+        );
+        return array_map(
+            fn($actor) => [
+                "name" => $actor["name"],
+                "character" => $actor["character"],
+                "profile_path" => $this->getUrl($actor["profile_path"], "w185"),
+                "id" => $actor["id"],
+            ],
+            $cast
+        );
     }
 
     /**
@@ -80,7 +105,9 @@ class MoviesService
      * @return string|null
      */
     private function getUrl(?string $path, string $size): ?string
-    { return $path ? "https://image.tmdb.org/t/p/{$size}{$path}" : null; }
+    {
+        return $path ? "https://image.tmdb.org/t/p/{$size}{$path}" : null;
+    }
 
     /**
      * Create or update a movie in the database.
@@ -90,41 +117,49 @@ class MoviesService
      */
     public function createOrUpdateMovie(array $movieData): void
     {
-        $videoId = $this->getYouTubeTrailerId($movieData['id']);
-        $existingGenres = $movieData['genre_ids'] ?? [];
-    
+        $videoId = $this->getYouTubeTrailerId($movieData["id"]);
+        $existingGenres = $movieData["genre_ids"] ?? [];
+
         $movie = Movie::updateOrCreate(
-            ['video_id' => $movieData['id']],
+            ["video_id" => $movieData["id"]],
             [
-                'title' => $movieData['title'],
-                'director' => $this->getDirector($movieData['id']),
-                'release_date' => isset($movieData['release_date']) ? date('Y-m-d', strtotime($movieData['release_date'])) : null,
-                'image' => $this->getUrl($movieData['poster_path'], 'w500'),
-                'overview' => $movieData['overview'] ?? null,
-                'backdrop_path' => $this->getUrl($movieData['backdrop_path'], 'original'),
-                'trailer_link' => $videoId,
-                'video_id' => $movieData['id'],
+                "title" => $movieData["title"],
+                "director" => $this->getDirector($movieData["id"]),
+                "release_date" => isset($movieData["release_date"])
+                    ? date("Y-m-d", strtotime($movieData["release_date"]))
+                    : null,
+                "image" => $this->getUrl($movieData["poster_path"], "w500"),
+                "overview" => $movieData["overview"] ?? null,
+                "backdrop_path" => $this->getUrl(
+                    $movieData["backdrop_path"],
+                    "original"
+                ),
+                "trailer_link" => $videoId,
+                "video_id" => $movieData["id"],
             ]
         );
-    
+
         $movie->genres()->sync($existingGenres);
-    
-        $castData = $this->getCast($movieData['id']);
+
+        $castData = $this->getCast($movieData["id"]);
         $castEntries = [];
         foreach ($castData as $actorData) {
             $castEntries[] = [
-                'movie_id' => $movie->id,
-                'actor_id' => $actorData['id'],
-                'name' => $actorData['name'],
-                'character' => $actorData['character'],
-                'profile_path' => $actorData['profile_path'],
-                'updated_at' => now(),
-                'created_at' => now(),
+                "movie_id" => $movie->id,
+                "actor_id" => $actorData["id"],
+                "name" => $actorData["name"],
+                "character" => $actorData["character"],
+                "profile_path" => $actorData["profile_path"],
+                "updated_at" => now(),
+                "created_at" => now(),
             ];
         }
-        MovieCast::upsert($castEntries, ['movie_id', 'actor_id'], ['name', 'character', 'profile_path']);
+        MovieCast::upsert(
+            $castEntries,
+            ["movie_id", "actor_id"],
+            ["name", "character", "profile_path"]
+        );
     }
-
 
     /**
      * Process and synchronize a single movie's data.
@@ -137,16 +172,28 @@ class MoviesService
      * @param bool $consoleOutput
      * @return void
      */
-    private function processMovieData(array $movieData, int &$syncCount, int $numberOfMoviesToDownload, int &$skipCount, int &$newCount, bool $consoleOutput): void
-    {
-        if (Movie::where('title', $movieData['title'])->exists()) {
-            $consoleOutput && printf("\033[K($syncCount/$numberOfMoviesToDownload) Movie '{$movieData['title']}' already exists.\033[35m Skip\033[0m.\n");
+    private function processMovieData(
+        array $movieData,
+        int &$syncCount,
+        int $numberOfMoviesToDownload,
+        int &$skipCount,
+        int &$newCount,
+        bool $consoleOutput
+    ): void {
+        if (Movie::where("title", $movieData["title"])->exists()) {
+            $consoleOutput &&
+                printf(
+                    "\033[K($syncCount/$numberOfMoviesToDownload) Movie '{$movieData["title"]}' already exists.\033[35m Skip\033[0m.\n"
+                );
             $skipCount++;
             return;
         }
         $this->createOrUpdateMovie($movieData);
         $newCount++;
-        $consoleOutput && printf("\033[K($syncCount/$numberOfMoviesToDownload) \033[33mNew\033[0m movie '{$movieData['title']}' added to the database.\n");
+        $consoleOutput &&
+            printf(
+                "\033[K($syncCount/$numberOfMoviesToDownload) \033[33mNew\033[0m movie '{$movieData["title"]}' added to the database.\n"
+            );
     }
 
     /**
@@ -158,24 +205,44 @@ class MoviesService
      * @throws GuzzleException
      */
 
-    public function fetchPopularMovies(int $numberOfMoviesToDownload, bool $consoleOutput=false): void
-    {
-        $consoleOutput && printf("\033[34m::\033[0m Synchronizing movies from TMDb...\n");
+    public function fetchPopularMovies(
+        int $numberOfMoviesToDownload,
+        bool $consoleOutput = false
+    ): void {
+        $consoleOutput &&
+            printf("\033[34m::\033[0m Synchronizing movies from TMDb...\n");
         $syncCount = $skipCount = $newCount = $page = $progresstmp = 0;
         $totalPages = ceil($numberOfMoviesToDownload / 20);
 
         while ($page++ < $totalPages) {
             $moviesData = $this->tmdbService->fetchMoviesData($page);
             foreach ($moviesData as $movieData) {
-                if ($syncCount >= $numberOfMoviesToDownload) { break 2; }
+                if ($syncCount >= $numberOfMoviesToDownload) {
+                    break 2;
+                }
                 $syncCount++;
-                $progress = floor(($syncCount / $numberOfMoviesToDownload) * 100);
+                $progress = floor(
+                    ($syncCount / $numberOfMoviesToDownload) * 100
+                );
                 //$bar = (str_repeat('#', floor($progress / 2)) . str_repeat('-', 50 - floor($progress / 2))); // default style
-                $bar = (str_repeat(' ', floor($progress / 2)) . ($progresstmp = $progresstmp == 'c' ? 'C' : 'c') . str_repeat('•', 50 - floor($progress / 2))); // pacman style
-                $this->processMovieData($movieData, $syncCount, $numberOfMoviesToDownload, $skipCount, $newCount, $consoleOutput);
+                $bar =
+                    str_repeat(" ", floor($progress / 2)) .
+                    ($progresstmp = $progresstmp == "c" ? "C" : "c") .
+                    str_repeat("•", 50 - floor($progress / 2)); // pacman style
+                $this->processMovieData(
+                    $movieData,
+                    $syncCount,
+                    $numberOfMoviesToDownload,
+                    $skipCount,
+                    $newCount,
+                    $consoleOutput
+                );
                 $consoleOutput && printf("\033[K[$bar] $progress%%\r");
             }
         }
-        $consoleOutput && printf("\033[KSuccessfully synchronized {$syncCount} movies.\nSkip: {$skipCount}\nNew: {$newCount}\n");
+        $consoleOutput &&
+            printf(
+                "\033[KSuccessfully synchronized {$syncCount} movies.\nSkip: {$skipCount}\nNew: {$newCount}\n"
+            );
     }
 }
--- app/Services/SeriesService.php
+++ app/Services/SeriesService.php
@@ -3,13 +3,13 @@
 namespace App\Services;
 
 use GuzzleHttp\Exception\GuzzleException;
-use App\Models\{Serie, SerieCast};
+use App\Models\{Serie, SerieCast, Genre};
 
 class SeriesService
 {
-    public function __construct(
-        protected TMDbService $tmdbService
-    ) {}
+    public function __construct(protected TMDbService $tmdbService)
+    {
+    }
 
     /**
      * Get the number of all series.
@@ -19,7 +19,10 @@ class SeriesService
      */
     public function getNumberOfAllSeries(): int
     {
-        return $this->tmdbService->fetchData('https://api.themoviedb.org/3/tv/popular', ['page' => 1])['total_results'] ?? 0;
+        return $this->tmdbService->fetchData(
+            "https://api.themoviedb.org/3/tv/popular",
+            ["page" => 1]
+        )["total_results"] ?? 0;
     }
 
     /**
@@ -31,12 +34,18 @@ class SeriesService
      */
     public function getYouTubeTrailerId(int $seriesId): ?string
     {
-        $videos = $this->tmdbService->fetchData("https://api.themoviedb.org/3/tv/{$seriesId}/videos", [])['results'] ?? [];
+        $videos =
+            $this->tmdbService->fetchData(
+                "https://api.themoviedb.org/3/tv/{$seriesId}/videos",
+                []
+            )["results"] ?? [];
+
         foreach ($videos as $video) {
-            if ($video['site'] === 'YouTube' && $video['type'] === 'Trailer') {
-                return $video['key'];
+            if ($video["site"] === "YouTube" && $video["type"] === "Trailer") {
+                return $video["key"];
             }
         }
+
         return null;
     }
 
@@ -49,13 +58,19 @@ class SeriesService
      */
     public function getDirector(int $seriesId): string
     {
-        $crew = $this->tmdbService->fetchData("https://api.themoviedb.org/3/tv/{$seriesId}/credits", [])['crew'] ?? [];
+        $crew =
+            $this->tmdbService->fetchData(
+                "https://api.themoviedb.org/3/tv/{$seriesId}/credits",
+                []
+            )["crew"] ?? [];
+
         foreach ($crew as $member) {
-            if ($member['job'] === 'Director') {
-                return $member['name'];
+            if ($member["job"] === "Director") {
+                return $member["name"];
             }
         }
-        return 'unknown director';
+
+        return "unknown director";
     }
 
     /**
@@ -67,13 +82,24 @@ class SeriesService
      */
     public function getCast(int $seriesId): array
     {
-        $cast = array_slice($this->tmdbService->fetchData("https://api.themoviedb.org/3/tv/{$seriesId}/credits", [])['cast'] ?? [], 0, 10);
-        return array_map(fn($actor) => [
-            'name' => $actor['name'],
-            'character' => $actor['character'],
-            'profile_path' => $this->getUrl($actor['profile_path'], 'w185'),
-            'id' => $actor['id']
-        ], $cast);
+        $cast = array_slice(
+            $this->tmdbService->fetchData(
+                "https://api.themoviedb.org/3/tv/{$seriesId}/credits",
+                []
+            )["cast"] ?? [],
+            0,
+            10
+        );
+
+        return array_map(
+            fn($actor) => [
+                "name" => $actor["name"],
+                "character" => $actor["character"],
+                "profile_path" => $this->getUrl($actor["profile_path"], "w185"),
+                "id" => $actor["id"],
+            ],
+            $cast
+        );
     }
 
     /**
@@ -96,36 +122,78 @@ class SeriesService
      */
     public function createOrUpdateSeries(array $seriesData): void
     {
-        $videoId = $this->getYouTubeTrailerId($seriesData['id']);
+        // Retrieve genres
+        $genreIds = $seriesData["genre_ids"] ?? [];
 
+        // Create or update series
         $series = Serie::updateOrCreate(
-            ['video_id' => $seriesData['id']],
+            ["video_id" => $seriesData["id"]],
             [
-                'title' => $seriesData['name'],
-                'director' => $this->getDirector($seriesData['id']),
-                'release_date' => isset($seriesData['first_air_date']) ? date('Y-m-d', strtotime($seriesData['first_air_date'])) : null,
-                'image' => $this->getUrl($seriesData['poster_path'], 'w500'),
-                'overview' => $seriesData['overview'] ?? null,
-                'backdrop_path' => $this->getUrl($seriesData['backdrop_path'], 'original'),
-                'trailer_link' => $videoId,
-                'video_id' => $seriesData['id'],
+                "title" => $seriesData["name"],
+                "director" => $this->getDirector($seriesData["id"]),
+                "release_date" => isset($seriesData["first_air_date"])
+                    ? date("Y-m-d", strtotime($seriesData["first_air_date"]))
+                    : null,
+                "image" => $this->getUrl($seriesData["poster_path"], "w500"),
+                "overview" => $seriesData["overview"] ?? null,
+                "backdrop_path" => $this->getUrl(
+                    $seriesData["backdrop_path"],
+                    "original"
+                ),
+                "trailer_link" => $this->getYouTubeTrailerId($seriesData["id"]),
+                "video_id" => $seriesData["id"],
             ]
         );
 
-        $castData = $this->getCast($seriesData['id']);
+        // Sync genres
+        $this->syncGenres($series, $genreIds);
+
+        // Sync cast
+        $this->syncCast($series, $this->getCast($seriesData["id"]));
+    }
+
+    /**
+     * Sync genres with the series.
+     *
+     * @param Serie $series
+     * @param array $genreIds
+     * @return void
+     */
+    private function syncGenres(Serie $series, array $genreIds): void
+    {
+        $existingGenreIds = Genre::whereIn("id", $genreIds)
+            ->pluck("id")
+            ->toArray();
+        $series->genres()->sync($existingGenreIds);
+    }
+
+    /**
+     * Sync cast with the series.
+     *
+     * @param Serie $series
+     * @param array $castData
+     * @return void
+     */
+    private function syncCast(Serie $series, array $castData): void
+    {
         $castEntries = [];
         foreach ($castData as $actorData) {
             $castEntries[] = [
-                'serie_id' => $series->id,
-                'actor_id' => $actorData['id'],
-                'name' => $actorData['name'],
-                'character' => $actorData['character'],
-                'profile_path' => $actorData['profile_path'],
-                'updated_at' => now(),
-                'created_at' => now(),
+                "serie_id" => $series->id,
+                "actor_id" => $actorData["id"],
+                "name" => $actorData["name"],
+                "character" => $actorData["character"],
+                "profile_path" => $actorData["profile_path"],
+                "updated_at" => now(),
+                "created_at" => now(),
             ];
         }
-        SerieCast::upsert($castEntries, ['serie_id', 'actor_id'], ['name', 'character', 'profile_path']);
+
+        SerieCast::upsert(
+            $castEntries,
+            ["serie_id", "actor_id"],
+            ["name", "character", "profile_path"]
+        );
     }
 
     /**
@@ -139,16 +207,29 @@ class SeriesService
      * @param bool $consoleOutput
      * @return void
      */
-    private function processSeriesData(array $seriesData, int &$syncCount, int $numberOfSeriesToDownload, int &$skipCount, int &$newCount, bool $consoleOutput): void
-    {
-        if (Serie::where('title', $seriesData['name'])->exists()) {
-            $consoleOutput && printf("\033[K($syncCount/$numberOfSeriesToDownload) Series '{$seriesData['name']}' already exists.\033[35m Skip\033[0m.\n");
+    private function processSeriesData(
+        array $seriesData,
+        int &$syncCount,
+        int $numberOfSeriesToDownload,
+        int &$skipCount,
+        int &$newCount,
+        bool $consoleOutput
+    ): void {
+        if (Serie::where("title", $seriesData["name"])->exists()) {
+            $consoleOutput &&
+                printf(
+                    "\033[K($syncCount/$numberOfSeriesToDownload) Series '{$seriesData["name"]}' already exists.\033[35m Skip\033[0m.\n"
+                );
             $skipCount++;
             return;
         }
+
         $this->createOrUpdateSeries($seriesData);
         $newCount++;
-        $consoleOutput && printf("\033[K($syncCount/$numberOfSeriesToDownload) \033[33mNew\033[0m series '{$seriesData['name']}' added to the database.\n");
+        $consoleOutput &&
+            printf(
+                "\033[K($syncCount/$numberOfSeriesToDownload) \033[33mNew\033[0m series '{$seriesData["name"]}' added to the database.\n"
+            );
     }
 
     /**
@@ -159,25 +240,44 @@ class SeriesService
      * @return void
      * @throws GuzzleException
      */
-
-    public function fetchPopularSeries(int $numberOfSeriesToDownload, bool $consoleOutput=false): void
-    {
-        $consoleOutput && printf("\033[34m::\033[0m Synchronizing series from TMDb...\n");
+    public function fetchPopularSeries(
+        int $numberOfSeriesToDownload,
+        bool $consoleOutput = false
+    ): void {
+        $consoleOutput &&
+            printf("\033[34m::\033[0m Synchronizing series from TMDb...\n");
         $syncCount = $skipCount = $newCount = $page = $progresstmp = 0;
         $totalPages = ceil($numberOfSeriesToDownload / 20);
 
         while ($page++ < $totalPages) {
             $seriesData = $this->tmdbService->fetchSeriesData($page);
             foreach ($seriesData as $serieData) {
-                if ($syncCount >= $numberOfSeriesToDownload) { break 2; }
+                if ($syncCount >= $numberOfSeriesToDownload) {
+                    break 2;
+                }
                 $syncCount++;
-                $progress = floor(($syncCount / $numberOfSeriesToDownload) * 100);
-                //$bar = (str_repeat('#', floor($progress / 2)) . str_repeat('-', 50 - floor($progress / 2))); // default style
-                $bar = (str_repeat(' ', floor($progress / 2)) . ($progresstmp = $progresstmp == 'c' ? 'C' : 'c') . str_repeat('•', 50 - floor($progress / 2))); // pacman style
-                $this->processSeriesData($serieData, $syncCount, $numberOfSeriesToDownload, $skipCount, $newCount, $consoleOutput);
+                $progress = floor(
+                    ($syncCount / $numberOfSeriesToDownload) * 100
+                );
+                $bar =
+                    str_repeat(" ", floor($progress / 2)) .
+                    ($progresstmp = $progresstmp == "c" ? "C" : "c") .
+                    str_repeat("•", 50 - floor($progress / 2)); // pacman style
+                $this->processSeriesData(
+                    $serieData,
+                    $syncCount,
+                    $numberOfSeriesToDownload,
+                    $skipCount,
+                    $newCount,
+                    $consoleOutput
+                );
                 $consoleOutput && printf("\033[K[$bar] $progress%%\r");
             }
         }
-        $consoleOutput && printf("\033[KSuccessfully synchronized {$syncCount} series.\nSkip: {$skipCount}\nNew: {$newCount}\n");
+
+        $consoleOutput &&
+            printf(
+                "\033[KSuccessfully synchronized {$syncCount} series.\nSkip: {$skipCount}\nNew: {$newCount}\n"
+            );
     }
 }
