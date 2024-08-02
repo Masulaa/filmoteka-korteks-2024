35c11b9f0b837542fa1ec71f44191b2c37719b7d
Luka <lukamasulovic2@gmail.com>
2024-08-02 18:02:21 +0200

    wrote tests for movies.index, movies.show and movies.store

--- database/factories/MovieFactory.php
+++ database/factories/MovieFactory.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace Database\Factories;
 
 use App\Models\Movie;
@@ -16,33 +15,14 @@ class MovieFactory extends Factory
         return [
             'title' => $this->faker->realText($this->faker->numberBetween(10, 25)),
             'director' => $this->faker->name,
-            'release_date' => $this->faker->date,
+            'release_date' => $this->faker->date(),
             'image' => $this->faker->imageUrl(),
+            'overview' => $this->faker->paragraph(),
             'trailer_link' => $this->faker->url(),
             'video_id' => $this->faker->randomNumber(),
             'backdrop_path' => $this->faker->imageUrl(),
-            'overview' => $this->faker->paragraph,
-            'views' => $this->faker->numberBetween(100, 10000),
         ];
     }
 
-    public function configure()
-    {
-        return $this->afterCreating(function (Movie $movie) {
-            // Proveri da li ima dovoljno žanrova, ako ne kreiraj ih
-            $genreCount = Genre::count();
-            if ($genreCount < 3) {
-                Genre::factory()->count(3 - $genreCount)->create();
-            }
 
-            // Pridruži nasumične žanrove filmu
-            $genres = Genre::all()->random(rand(1, 3))->pluck('id');
-            $movie->genres()->attach($genres);
-
-            // Kreiraj ocene za film
-            MovieRating::factory()->count(5)->create([
-                'movie_id' => $movie->id,
-            ]);
-        });
-    }
 }
--- tests/Feature/MovieTest.php
+++ tests/Feature/MovieTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Models\Movie;
+use App\Models\User; // Dodajte ovaj use statement
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -12,12 +13,71 @@ class MovieTest extends TestCase
 
     public function test_movies_are_displayed(): void
     {
-        $movies = Movie::factory()->create();
+        $user = User::factory()->create();
 
-        $response = $this
-            ->actingAs($movies)
-            ->get('/movies');
+        $this->actingAs($user);
+
+        $response = $this->get('movies');
 
         $response->assertOk();
+    }
+
+    public function test_single_movie_is_displayed(): void
+    {
+
+        $user = User::factory()->create();
+
+        $this->actingAs($user);
+
+        $movie = Movie::factory()->create([
+            'title' => 'Test Movie',
+            'overview' => 'This is a test movie description.',
+            'director' => "Majkl Dzordan",
+            'release_date' => "2012-03-07",
+            'image' => "testimage.url",
+            'video_id' => "100",
+            'backdrop_path' => "backdroppath.url",
+        ]);
+
+        $response = $this->get(route('movies.show', $movie->id));
+
+        $response->assertOk();
+
+        $response->assertSee($movie->title);
+        $response->assertSee($movie->overview);
+        $response->assertSee($movie->director);
+        $response->assertSee($movie->release_date);
+        $response->assertSee($movie->image);
+        $response->assertSee($movie->video_id);
+        $response->assertSee($movie->backdrop_path);
+    }
+
+    public function test_movie_can_be_created(): void
+    {
+
+
+        $user = User::factory()->create(['is_admin' => 1]);
+
+        $this->actingAs($user);
+
+
+        $response = $this->post(route('admin.movies.store'), [
+            'title' => 'New Movie',
+            'director' => 'Director Name',
+            'release_date' => '2023-01-01',
+            'views' => 150,
+        ]);
+
+        $response->assertRedirect(route('admin.movies.index'));
+
+        // Proverite da li je film sačuvan u bazi
+        $this->assertDatabaseHas('movies', [
+            'title' => 'New Movie',
+            'director' => 'Director Name',
+            'release_date' => '2023-01-01',
+
+        ]);
+
+
     }
 }
