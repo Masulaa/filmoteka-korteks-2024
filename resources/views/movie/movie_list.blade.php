8ee78cd0531905c166aedfc9b85b5cb16e96951f
Luka <lukamasulovic2@gmail.com>
2024-08-03 23:42:56 +0200

    added new image code to movies and series search

--- resources/views/movie/movie_list.blade.php
+++ resources/views/movie/movie_list.blade.php
@@ -1,7 +1,13 @@
 @foreach ($movies as $movie)
     <li>
         <a href="{{ route('movies.show', $movie->id) }}" class="flex gap-2">
-            <img width="56px" src="{{ $movie->image }}">
+        @php
+        $imageUrl = "https://image.tmdb.org/t/p/w500/{$movie->image}";
+        $fallbackImage = "storage/movies-images/{$movie->image}";
+        $imageExists = @getimagesize($imageUrl);
+        $displayImage = $imageExists ? $imageUrl : $fallbackImage;
+        @endphp
+            <img width="56px" src="{{ $displayImage }}">
             <div class="flex flex-col">
                 <h1 class="text-lg font-bold">{{ $movie->title }}</h1>
                 <span>{{ $movie->director }} | {{ $movie->release_date }}</span>
--- resources/views/serie/serie_list.blade.php
+++ resources/views/serie/serie_list.blade.php
@@ -1,7 +1,13 @@
 @foreach ($series as $serie)
     <li>
         <a href="{{ route('series.show', $serie->id) }}" class="flex gap-2">
-            <img width="56px" src="https://image.tmdb.org/t/p/w500/{{ $serie->image }}">
+        @php
+        $imageUrl = "https://image.tmdb.org/t/p/w500/{$serie->image}";
+        $fallbackImage = "storage/series-images/{$serie->image}";
+        $imageExists = @getimagesize($imageUrl);
+        $displayImage = $imageExists ? $imageUrl : $fallbackImage;
+        @endphp
+            <img width="56px" src="{{ $displayImage }}">
             <div class="flex flex-col">
                 <h1 class="text-lg font-bold">{{ $serie->title }}</h1>
                 <span>{{ $serie->director }} | {{ $serie->release_date }}</span>
